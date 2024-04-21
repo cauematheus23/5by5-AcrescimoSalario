9d32c6cbedb54bcf93f3830329c2772446946ac0
cauematheus <cauematheus_23@hotmail.com>
2024-04-21 19:08:08 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioAcrescimoSalario/Program.cs
@@ -0,0 +1,19 @@
+﻿float s, a, pa;
+do
+{
+    Console.WriteLine("Digite o salario normal: ");
+    s = float.Parse(Console.ReadLine());
+} while (s <= 0);
+
+do
+{
+    Console.WriteLine("Digite o salario com aumento: ");
+    a = float.Parse(Console.ReadLine());
+} while (a <= 0 || a <= s);
+
+pa = a - s;
+pa = pa / s;
+pa = pa * 100;
+Console.WriteLine($"O salario anterior era de R${s} e depois de um aumento de {pa}% foi para R${a}");
+
+
